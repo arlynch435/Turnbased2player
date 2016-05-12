85dae2ac4e2ddf33d8af0e89999347862035e8af
arlynch435 <ARLynch@student.naperville203.org>
2016-05-12 14:49:33 -0500

    started working with abstract stuff

---
+++ 2 player/Player.java
@@ -0,0 +1,36 @@
+
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class Player
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private String name;
+
+    /**
+     * Default constructor for objects of class Player
+     */
+    public Player(String title)
+    {
+        // initialise instance variables
+        this.name=title;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public abstract void action();
+
+}
